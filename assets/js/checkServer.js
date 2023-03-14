39a00ac839abe2e9ae6c584773694d085b4c7c33
Taka005 <01taka005@gmail.com>
2023-03-14 21:40:55 +0900

    修正

--- assets/js/checkServer.js
+++ assets/js/checkServer.js
@@ -6,8 +6,8 @@ serverForm.addEventListener("submit",()=>{
     if(serverInput.value.length == 0){
         event.preventDefault();
         return alert("作成するサーバー名を入力してください");
-    }else if(serverInput.value.length > 30){
+    }else if(serverInput.value.length > 15){
         event.preventDefault();
-        return alert("作成するサーバー名は30文字以内にしてください");
+        return alert("作成するサーバー名は15文字以内にしてください");
     }
 });
