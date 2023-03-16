30434bfdfc507de79a8f9333becc40fe2bf85605
Taka005 <01taka005@gmail.com>
2023-03-16 22:41:52 +0900

    修正

--- assets/js/checkMessage.js
+++ assets/js/checkMessage.js
@@ -6,8 +6,8 @@ messageForm.addEventListener("submit",()=>{
     if(messageInput.value.length == 0){
         event.preventDefault();
         return alert("メッセージを入力してください");
-    }else if(messageInput.value.length > 300){
+    }else if(messageInput.value.length > 150){
         event.preventDefault();
-        return alert("送信するメッセージはは300文字以内にしてください");
+        return alert("送信するメッセージはは150文字以内にしてください");
     }
 });
--- assets/js/checkServer.js
+++ assets/js/checkServer.js
@@ -6,8 +6,8 @@ serverForm.addEventListener("submit",()=>{
     if(serverInput.value.length == 0){
         event.preventDefault();
         return alert("作成するサーバー名を入力してください");
-    }else if(serverInput.value.length > 15){
+    }else if(serverInput.value.length > 10){
         event.preventDefault();
-        return alert("作成するサーバー名は15文字以内にしてください");
+        return alert("作成するサーバー名は10文字以内にしてください");
     }
 });
