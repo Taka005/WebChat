f963cbe26ce2b018036225cafc4bfed121d5a17d
Taka005 <01taka005@gmail.com>
2023-03-16 22:26:38 +0900

    修正

--- system/lib.php
+++ system/lib.php
@@ -13,17 +13,17 @@ function createId($n){
     return $random;
 }
 
-function mb_wordwrap($string, $width=75, $break="\n", $cut = false) {
-    if (!$cut) {
+function mb_wordwrap($string, $width=75, $break="\n", $cut = false){
+    if(!$cut){
         $regexp = "#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){".$width.",}\b#U";
-    } else {
+    }else{
         $regexp = "#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){".$width."}#";
     }
     $string_length = mb_strlen($string,"UTF-8");
     $cut_length = ceil($string_length / $width);
     $i = 1;
-    $return = '';
-    while ($i < $cut_length) {
+    $return = "";
+    while ($i < $cut_length){
         preg_match($regexp, $string, $matches);
         $new_string = (!$matches ? $string : $matches[0]);
         $return .= $new_string.$break;
