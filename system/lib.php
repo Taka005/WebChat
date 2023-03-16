e1658e069af5c29909c3f39c0ede7eb1e9376a94
Taka005 <01taka005@gmail.com>
2023-03-16 22:20:15 +0900

    修正

--- system/lib.php
+++ system/lib.php
@@ -13,6 +13,26 @@ function createId($n){
     return $random;
 }
 
+function mb_wordwrap($string, $width=75, $break="\n", $cut = false) {
+    if (!$cut) {
+        $regexp = "#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){".$width.",}\b#U";
+    } else {
+        $regexp = "#^(?:[\x00-\x7F]|[\xC0-\xFF][\x80-\xBF]+){".$width."}#";
+    }
+    $string_length = mb_strlen($string,"UTF-8");
+    $cut_length = ceil($string_length / $width);
+    $i = 1;
+    $return = '';
+    while ($i < $cut_length) {
+        preg_match($regexp, $string, $matches);
+        $new_string = (!$matches ? $string : $matches[0]);
+        $return .= $new_string.$break;
+        $string = substr($string, strlen($new_string));
+        $i++;
+    }
+    return $return.$string;
+}
+
 //システム
 //ユーザー
 
