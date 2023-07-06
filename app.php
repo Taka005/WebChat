710afe34054bee7d2ea1f811ca000428f39e3608
Taka005 <01taka005@gmail.com>
2023-07-06 17:02:18 +0900

    Update app.php

--- app.php
+++ app.php
@@ -49,12 +49,12 @@ if(!empty($server)){
         <meta name="theme-color" content="#ffffff">
 
         <head prefix="og: https://ogp.me/ns# fb: https://ogp.me/ns/ fb# prefix属性: https://ogp.me/ns/ prefix属性#">
-        <meta property="og:url" content="https://chat.takadev.tk/" />
+        <meta property="og:url" content="https://chat.taka.ml/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="チャット" />
         <meta property="og:description" content="Web上で使えるオープンチャットツール" />
         <meta property="og:site_name" content="WebChat" />
-        <meta property="og:image" content="https://chat.takadev.tk/assets/img/icon.png" />
+        <meta property="og:image" content="https://chat.taka.ml/assets/img/icon.png" />
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="./assets/css/main.css">
